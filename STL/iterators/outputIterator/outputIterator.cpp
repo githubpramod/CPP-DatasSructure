20d7c5ab57fcdb4afe88983375ea81b20e3fa4c6
pramod.emb645@gmail.com <pramod.emb645@gmail.com>
2020-02-23 22:28:54 +0100

    Non Modified algorithms!

---
+++ STL/iterators/outputIterator/outputIterator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+#include<numeric>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+	istream_iterator<double> end;
+	istream_iterator<double> din(cin);
+
+	vector<double> doubles;
+	back_insert_iterator<vector<double>> bins(doubles);
+
+	while(din!=end) {
+		*bins++=*din++;
+	}
+
+	partial_sum(doubles.begin(),doubles.end(),ostream_iterator<double>(cout, " "));
+	cout<<endl;
+
+	return 0;
+}
